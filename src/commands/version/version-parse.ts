fdacc7872242726954754caaf6197717129cf640
Josef Karamon <josef.karamon@gmail.com>
2025-08-06 16:36:43 +0200

    Bump version to 1.0.16 and enhance version parsing to include majorMinorPatchPre output

--- src/commands/version/version-parse.ts
+++ src/commands/version/version-parse.ts
@@ -23,11 +23,18 @@ export class VersionParse extends BaseCommand<VersionParseArgs> {
       core.setFailed(`Invalid version format: ${rawVersion}`);
       return;
     }
+    const preString = parsedVersion.pre ? parsedVersion.pre.join('.') : '';
     core.info(`Parsed version: ${JSON.stringify(parsedVersion)}`);
     core.setOutput('major', parsedVersion.major?.toString());
     core.setOutput('minor', parsedVersion.minor?.toString());
     core.setOutput('patch', parsedVersion.patch?.toString());
     core.setOutput('build', parsedVersion.build || '');
-    core.setOutput('pre', parsedVersion.pre ? parsedVersion.pre.join('.') : '');
+    core.setOutput('pre', preString);
+    core.setOutput('majorMinor', `${parsedVersion.major}.${parsedVersion.minor}`);
+    core.setOutput('majorMinorPatch', `${parsedVersion.major}.${parsedVersion.minor}.${parsedVersion.patch}`);
+    core.setOutput(
+      'majorMinorPatchPre',
+      `${parsedVersion.major}.${parsedVersion.minor}.${parsedVersion.patch}${preString ? `-${preString}` : ''}`
+    );
   }
 }
